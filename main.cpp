ef33c2bed4ac29e82f8fda9d2d992d2fcdfc9e81
Varun <varunsom@outlook.com>
2018-03-29 12:18:10 -0400

    add os detection feature

--- main.cpp
+++ main.cpp
@@ -13,10 +13,33 @@ int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Shinemanager version 0 (dev) \n";
 
-    if (argc <= 1) {
+    if (argc <= 2) {
       std::cout << "USAGE for shine \nAvailable commands: \n\n";
       for (size_t i = commandsAmt; i--;) {
         std::cout << commandsDesc[i] << "\n";
+      }
+      std::cout << std::endl;
+    }
+    if (argc == 3) {
+      // Handle install or uninstall
+      if (strcmp(argv[1], "install") == 0) {
+
+          std::cout << "installing stuf..." << '\n';
+        // Detect OS for running install script.
+        #ifdef _WIN32
+          /* TODO: Implement #ifdef _WIN64 for detecting 32-bit and
+           64-bit operating systems. */
+          std::cout << "You're running Windows!" << '\n';
+        #elif __APPLE__
+          std::cout << "You're running macOS!" << '\n';
+        #elif __linux__
+          std::cout << "You're running Linux!" << '\n';
+        #elif __unix__
+          std::cout << "You're running UNIX!" << '\n';
+        #else
+          std::cout << "Unknown OS version. Quitting." << '\n';
+          return 0;
+        #endif
       }
     }
 
