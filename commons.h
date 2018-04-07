eda16740a17159d436e46efe543a7b9530c2be9d
Varun <varunsom@outlook.com>
2018-04-07 07:30:19 -0400

    Add some variables to check if user wants to install or abort

---
+++ commons.h
@@ -0,0 +1,26 @@
+//
+//  commons.h
+//  PackMan
+//
+//  Created by Varun Somashekar on 3/28/18.
+//  Copyright © 2018 Varun Somashekar. All rights reserved.
+//
+//  This file contains global variables
+//
+
+#include<iostream>
+
+const int COMMANDS_AMT = 2;
+
+const std::string COMMANDS_DESC[COMMANDS_AMT] = {
+  "uninstall - uninstall a package: shine uninstall <PACKAGE_NAME>",
+  "install - install a package: shine install <PACKAGE_NAME>"
+};
+
+const std::string COMMANDS_DB[COMMANDS_AMT] = {
+  "uninstall",
+  "install"
+};
+
+std::string installItem;
+std::string yesOrNo;
