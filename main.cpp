bf67dc0aa430896a98d2259cea2bfe84bf203576
Varun <varunsom@outlook.com>
2018-04-07 07:31:24 -0400

    Further the install process

---
+++ main.cpp
@@ -0,0 +1,47 @@
+//
+//  main.cpp
+//  Shine package manager
+//
+//  Created by Varun Somashekar.
+//  Copyright © 2018 Varun Somashekar. All rights reserved.
+//
+
+#include <iostream>
+#include "commons.h"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Shinemanager version 0 (dev) \n";
+
+    if (argc <= 2) {
+      std::cout << "USAGE for shine \nAvailable commands: \n\n";
+      for (size_t i = COMMANDS_AMT; i--;) {
+        std::cout << COMMANDS_DESC[i] << "\n";
+      }
+      std::cout << std::endl;
+    }
+    if (argc == 3) {
+      // Handle install or uninstall
+      if (strcmp(argv[1], "install") == 0) {
+
+          std::cout << "installing stuff..." << '\n';
+        // Detect OS for running install script.
+        #ifdef _WIN32
+          /* TODO: Implement #ifdef _WIN64 for detecting 32-bit and
+           64-bit operating systems. */
+          std::cout << "You're running Windows!" << '\n';
+        #elif __APPLE__
+          std::cout << "You're running macOS!" << '\n';
+        #elif __linux__
+          std::cout << "You're running Linux!" << '\n';
+        #elif __unix__
+          std::cout << "You're running UNIX!" << '\n';
+        #else
+          std::cout << "Unknown OS version. Quitting." << '\n';
+          return 0;
+        #endif
+      }
+    }
+
+    return 0;
+}
