edf77d0e1f7fffd39b625e9942a15fc1bc9786bd
Varun <varunsom@outlook.com>
2018-04-07 07:27:02 -0400

    Add a new repo
    Added nodejs

--- install_shine.sh
+++ install_shine.sh
@@ -4,14 +4,13 @@ echo "This script will get Shine set up and installed on your computer."
 # Check if g++ is installed
 echo "Checking if g++ is installed"
 
-if [ -f /usr/bin/g++ ] then
-  echo "g++ compiler found. Setting up your environment..."
-  git clone https://github.com/flash76/shine
-  cd shine/
+if [ -f /usr/bin/g++ ]
+then
+  echo "g++ compiler found. Setting up your environment..."; &> git clone https://github.com/flash76/shine &> /dev/null
+  cd shine
 
   # Compile Shine
-  echo "Compiling Shine..."
-  g++ -o shine main.cpp
+  g++ -o shine main.cpp > "Compiling Shine"
 
   # Set up the environment
   export PATH=`pwd`:$PATH
