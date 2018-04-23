b35e80aee0f973076f36787ceaad3c7e6be6e5f2
flash76 <Varun@Veenas-MacBook-Pro.local>
2018-04-23 17:27:40 -0400

    Rewrite the statement that checks if user answered yes or no to if else

--- shine/main.cpp
+++ shine/main.cpp
@@ -9,42 +9,46 @@
 #include <iostream>
 #include "commons.h"
 
-
-int main(int argc, const char * argv[]) {
+int main(int argc, const char *argv[])
+{
     // insert code here...
     std::cout << "Shinemanager version 0 (dev) \n";
 
-    if (argc <= 2) {
-      std::cout << "USAGE for shine \nAvailable commands: \n\n";
-      for (size_t i = COMMANDS_AMT; i--;) {
-        std::cout << COMMANDS_DESC[i] << "\n";
-      }
-      std::cout << std::endl;
+    if (argc <= 2)
+    {
+        std::cout << "USAGE for shine \nAvailable commands: \n\n";
+        for (size_t i = COMMANDS_AMT; i--;)
+        {
+            std::cout << COMMANDS_DESC[i] << "\n";
+        }
+        std::cout << std::endl;
     }
-    if (argc == 3) {
-      // Handle install or uninstall
-      if (strcmp(argv[1], "install") == 0) {
-          std::cout << "installing stuff..." << '\n';
-          while (getline(repoStream, line)) {
-            repoStream >> line;
-            std::cout << line << '\n';
+    if (argc == 3)
+    {
+        // Handle install or uninstall
+        if (strcmp(argv[1], "install") == 0)
+        {
+            std::cout << "installing stuff..." << '\n';
+            std::getline(std::cin, yesOrNo);
             // Check if input is yes or no
-            switch (yesOrNo) {
-              case "yes":
+            if (yesOrNo.compare("yes") == 0) {
                 // Commence install
                 std::cout << "Commencing install process..." << '\n';
-              case "y":
+            }
+            else if (yesOrNo.compare("y") == 0) {
                 // Commence install
                 std::cout << "Commencing install process..." << '\n';
-              case "no":
+            }
+            else if (yesOrNo.compare("no") == 0) { 
                 // Cancel install
                 std::cout << "Canceling install. Goodbye!" << '\n';
-              case "n":
+            }
+            else if (yesOrNo.compare("n") == 0) { 
                 // Cancel install
                 std::cout << "Canceling install. Goodbye!" << '\n';
-          }
-      }
-    }
+            }
+        }
 
     return 0;
+}
 }
